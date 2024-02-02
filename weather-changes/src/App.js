04dfff1e7b020be681a4d66035ce976165bf8072
kavyaFullstackDeveloper <kavya.why@gmail.com>
2024-02-02 15:11:28 +0530

    weather app

---
+++ weather-changes/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import EquestrianWeather from '../src/EquestrianWeather'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <EquestrianWeather/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
